838be7fca48634e626161b2e5f0c227cbef952d6
srinivasa-dev <sriyadav98@gmail.com>
2023-04-25 19:17:47 +0530

    Integrated Firebase Performance Feature

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,6 +1,7 @@
  // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-analytics.js";
+import { getPerformance } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-performance.js";
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from "./apikeys.js";
 
 // Your web app's Firebase configuration
@@ -15,6 +16,6 @@ const firebaseConfig = {
   measurementId: MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const perf = getPerformance(app);
