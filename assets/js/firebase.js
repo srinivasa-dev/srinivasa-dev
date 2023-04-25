c24128216157e1ef5bb45818cb07e035714fd252
srinivasa-dev <sriyadav98@gmail.com>
2023-04-25 16:16:16 +0530

    Integrated Firebase for analytics

---
+++ assets/js/firebase.js
@@ -0,0 +1,20 @@
+ // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-analytics.js";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from "./apikeys.js";
+
+// Your web app's Firebase configuration
+// API KEYS are hidden reference https://www.youtube.com/watch?v=2J3xbMkH2K4
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
